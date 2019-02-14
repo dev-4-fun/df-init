3f8b5671d7c1600130920162415a0961923bb309
Dmitry Fiodorov <fedor.dmitry@gmail.com>
2019-02-14 23:50:46 +0300

    project cleanup

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 const logger = require('./utils/logger');
-const greeter = (name) => `Hello, ${name}!`;
 
 logger.debug('Hello world', 7, { hello: 'world' }, null, ['a', 'b', 'c']);
-module.exports = {
-  greeter
-};
