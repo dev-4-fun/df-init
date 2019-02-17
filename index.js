5ef4b745d9bb06022d8a28bcb7a92fd227ef6f3e
Dmitry Fiodorov <fedor.dmitry@gmail.com>
2019-02-17 19:27:45 +0300

    creating dir and package.json

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-const logger = require('./utils/logger');
-
-logger.debug('Hello world', 7, { hello: 'world' }, null, ['a', 'b', 'c']);
+const fs = require('fs');
+const path = require('path');
+const destDirName = process.argv[2];
+let realPath = path.resolve(__dirname);
+if (destDirName) {
+  realPath = path.join(realPath, destDirName);
+  fs.mkdirSync(realPath);
+}
+let packageName = realPath.split(path.sep).slice(-1).join();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  if (data.trim().length > 0) {
+    packageName = data.trim();
+  }
+  process.stdin.pause();
+  initPackage();
+});
+function initPackage() {
+  if (fs.readdirSync(realPath).length === 0) {
+    if (packageName === '\n') {
+      packageName = realPath.split(path.sep).slice(-1).join();
+    }
+    const packageJson = {
+      name: packageName,
+      author: 'Gribadze <fedor.dmitry@gmail.com>',
+      license: 'MIT',
+    };
+    fs.writeFileSync(
+      path.join(realPath, 'package.json'),
+      JSON.stringify(packageJson, null, 2),
+    );
+  }
+}
