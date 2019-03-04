a9f27a8556f6eaeefa2121797ad8bc6cfb600b80
Dmitry Fiodorov <fedor.dmitry@gmail.com>
2019-03-04 13:15:36 +0300

    cleanup

--- services/githubApi.test.js
+++ services/githubApi.test.js
@@ -2,7 +2,8 @@ const githubApi = require('./githubApi');
 
 describe('githubApi', () => {
   test('echo test', async () => {
+    githubApi.test = githubApi.test || function () { };
     const data = await githubApi.test();
     expect(data).toMatch(/^.*\.$/);
-  })
-})
+  });
+});
